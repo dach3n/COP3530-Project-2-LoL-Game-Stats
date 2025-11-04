3db0a4e81f1851bce5d6fee690277f6928abebc8
dach3n <chen.david@ufl.edu>
2025-11-03 23:23:09 -0500

    Configure catch2 testing

--- test/test.cpp
+++ test/test.cpp
@@ -1,13 +1,13 @@
-// #include <catch2/catch_test_macros.hpp>
-// #include <iostream>
-//
-// // uncomment and replace the following with your own headers
-// // David Chen 97345471
-// #include <algorithm>
-//
-// #include "quicksort.h"
-// #include "catch2/internal/catch_windows_h_proxy.hpp"
-//
-// using namespace std;
-//
+#include <catch2/catch_test_macros.hpp>
+#include <iostream>
+
+// uncomment and replace the following with your own headers
+// David Chen 97345471
+#include <algorithm>
+
+#include "quicksort.h"
+#include "catch2/internal/catch_windows_h_proxy.hpp"
+
+using namespace std;
+
 
