d9d2c556cb7d8f1761b2d95a40d7b64c2219a38d
dach3n <chen.david@ufl.edu>
2025-11-04 01:16:19 -0500

    Create main and create TODO list

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+//
+// Created by kingd on 11/4/2025.
+//
+
+#include <vector>
+#include <iostream>
+#include "quicksort.h"
+#include "heapsort.h"
+using namespace std;
+
+int main() {
+    // TODO
+    // read data from LoL csv
+    // enter user interface loop
+        /* GUI menu
+         * sort by game time
+         * sort by total game kills
+         * sort by winning team kill difference
+         * sort by total game gold
+         * sort by winning team gold difference
+         * sort by total damage dealt
+         */
+        // new screen: graph with data displayed
+        // buttons for mean, median, mode, range, etc.
+        // buttons to return to graph selection, quit
+    return 0;
+}
