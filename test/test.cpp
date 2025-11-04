b754a023731049aa380e6752a2c98b9af97b3a02
dach3n <chen.david@ufl.edu>
2025-11-04 00:19:00 -0500

    Create tests for trivial array and large number of elements

--- test/test.cpp
+++ test/test.cpp
@@ -4,10 +4,32 @@
 // uncomment and replace the following with your own headers
 // David Chen 97345471
 #include <algorithm>
+#include <random>
 
 #include "quicksort.h"
 #include "catch2/internal/catch_windows_h_proxy.hpp"
 
 using namespace std;
 
+TEST_CASE("Trivial Array", "[given]") {
+    vector<int> arr = {3, 2, 1, 5, 4, 7, 9, 6, 8};
+    myQuickSort(arr, 0, arr.size() - 1);
+    vector<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    REQUIRE(arr == expected);
+}
 
+
+TEST_CASE("Big Quick Sort" , "[given]") {
+    vector<int> arr;
+    for (int i = 0; i < 100000; i++) {
+        arr.push_back((i + 1000 + rand() % 9000));
+    }
+    vector<int> test = arr;
+
+    myQuickSort(arr, 0, arr.size()-1);
+    sort(test.begin(), test.end());
+    for (int i = 0; i < static_cast<int>(arr.size()); i++) {
+        cout << i << ": " << test[i] << " | " << arr[i] << endl;
+        REQUIRE(arr[i] == test[i]);
+    }
+}
