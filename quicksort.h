e57972086bbf78d7895b128a32083ac617a15307
dach3n <chen.david@ufl.edu>
2025-11-03 23:09:01 -0500

    Implement quicksort using median-of-three partitions

--- quicksort.h
+++ quicksort.h
@@ -5,6 +5,53 @@
 #ifndef COP3530_PROJECT_2_LOL_GAME_STATS_QUICKSORT_H
 #define COP3530_PROJECT_2_LOL_GAME_STATS_QUICKSORT_H
 
+#include <cmath>
+#include <vector>
+using namespace std;
 
+inline int partition(vector<int*> arr, const int low, const int high) {
+    // Choose pivot as median of three
+    const int mid = floor((low + high) / 2);
+    if (*(arr[mid]) < *(arr[low])) {
+        swap(arr[low], arr[mid]);
+    }
+    if (*(arr[high]) < *(arr[low])) {
+        swap(arr[low], arr[high]);
+    }
+    if (*(arr[mid]) < *(arr[high])) {
+        swap(arr[mid], arr[high]);
+    }
+    int pivot = *(arr[high]);
+    int up = low, down = high;
+
+    while (up < down) {
+        for (int i = up; i < high; i++) {
+            if (*(arr[up]) > pivot) {
+                break;
+            }
+            up++;
+        }
+        for (int i = high; i > low; i--) {
+            if (*(arr[down]) < pivot) {
+                break;
+            }
+            down--;
+        }
+        if (up < down) {
+            swap(arr[up], arr[down]);
+        }
+    }
+    swap(arr[low], arr[down]);
+    return down;
+
+}
+
+inline void quickSort(const vector<int*>& arr, const int low, const int high) {
+    if (low < high) {
+        const int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot-1);
+        quickSort(arr, pivot+1, high);
+    }
+}
 
 #endif //COP3530_PROJECT_2_LOL_GAME_STATS_QUICKSORT_H
