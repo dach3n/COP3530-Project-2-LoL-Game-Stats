f3c038556991d3adfc8d81c8bf89d29dd1915aca
dach3n <chen.david@ufl.edu>
2025-11-04 01:03:29 -0500

    Fix merged test files

--- test/test.cpp
+++ test/test.cpp
@@ -15,7 +15,7 @@ using namespace std;
 TEST_CASE("Trivial Array", "[heapsort]") {
     vector<int> arr = {3, 2, 5, 4, 6, 1, 9, 7, 8};
     vector<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    heapSort(arr);
+    myHeapSort(arr);
     REQUIRE(arr == expected);
 }
 
@@ -25,9 +25,15 @@ TEST_CASE("Big Heap Sort", "[heapsort]") {
         arr.push_back(i + 1000 + rand() % 9000);
     }
     vector<int> test = arr;
-    heapSort(arr);
-  
-TEST_CASE("Trivial Array", "[given]") {
+    myHeapSort(arr);
+    sort(test.begin(), test.end());
+    for (int i = 0; i < static_cast<int>(arr.size()); i++) {
+        cout << i << ": " << test[i] << " | " << arr[i] << endl;
+        REQUIRE(arr[i] == test[i]);
+    }
+}
+
+TEST_CASE("Trivial Array", "[quicksort]") {
     vector<int> arr = {3, 2, 1, 5, 4, 7, 9, 6, 8};
     myQuickSort(arr, 0, arr.size() - 1);
     vector<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -35,7 +41,7 @@ TEST_CASE("Trivial Array", "[given]") {
 }
 
 
-TEST_CASE("Big Quick Sort" , "[given]") {
+TEST_CASE("Big Quick Sort" , "[quicksort]") {
     vector<int> arr;
     for (int i = 0; i < 100000; i++) {
         arr.push_back((i + 1000 + rand() % 9000));
