63d8ca0c5acc156b383772a3485ee022d0809896
shark524 <ryan.anteen@ufl.edu>
2025-11-08 17:25:25 -0500

    You may need to download SFML to run the program. I am unsure

---
+++ src/GUI.h
@@ -0,0 +1,10 @@
+//
+// Created by rjant on 11/8/2025.
+//I WILL be making supporting functions for the GUI in here
+
+#ifndef GUI_H
+#define GUI_H
+
+#endif //GUI_H
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 #include <vector>
 #include <iostream>
+#include <SFML/Graphics.hpp>
 #include "quicksort.h"
 #include "heapsort.h"
 using namespace std;
@@ -12,6 +13,18 @@ int main() {
     // TODO
     // read data from LoL csv
     // enter user interface loop
+    sf::RenderWindow welcome(sf::VideoMode(1000, 1000), "Welcome");
+    while(welcome.isOpen()) {
+        sf::Event event;
+        while(welcome.pollEvent(event)) {
+            if(event.type == sf::Event::Closed) {
+                welcome.close();
+            }
+
+        }
+        welcome.clear(sf::Color::Blue);
+        welcome.display();
+    }
         /* GUI menu
          * sort by game time
          * sort by total game kills
