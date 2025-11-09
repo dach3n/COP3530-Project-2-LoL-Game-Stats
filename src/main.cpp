f0481ac9ce2ae68e4df1e0e58e235d2e5f6fd3c8
dach3n <chen.david@ufl.edu>
2025-11-08 20:17:29 -0500

    Change SFML main loop work with SFML 3.0.2

--- src/main.cpp
+++ src/main.cpp
@@ -7,32 +7,34 @@
 #include <SFML/Graphics.hpp>
 #include "quicksort.h"
 #include "heapsort.h"
-using namespace std;
+
 
 int main() {
     // TODO
     // read data from LoL csv
     // enter user interface loop
-    sf::Texture backgroundTexture;
-    backgroundTexture.loadFromFile("files/images/LOL_BG.jpg");
-    sf::Sprite backgroundSprite(backgroundTexture);
-    sf::RenderWindow welcome(sf::VideoMode(1000, 1000), "Welcome");
-    float scaleX = static_cast<float>(welcome.getSize().x) / backgroundTexture.getSize().x;
-    float scaleY = static_cast<float>(welcome.getSize().y) / backgroundTexture.getSize().y;
-    backgroundSprite.setScale(scaleX, scaleY);
 
-    while(welcome.isOpen()) {
-        sf::Event event;
-        while(welcome.pollEvent(event)) {
-            if(event.type == sf::Event::Closed) {
-                welcome.close();
-            }
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
 
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        while (const std::optional event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
         }
-        welcome.clear();
-        welcome.draw(backgroundSprite);
-        welcome.display();
+
+        // Clear screen
+        window.clear();
+
+        // Update the window
+        window.display();
     }
+
         /* GUI menu
          * sort by game time
          * sort by total game kills
