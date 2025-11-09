02dc93903ae7c0afd0bb956c964bf48a5d4662af
David Chen <chen.david@ufl.edu>
2025-11-08 23:21:58 -0500

    Delete cmake-build-debug directory
    Not supposed to have this here

--- cmake-build-debug/CMakeFiles/fc-tmp/sfml/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing update step for sfml")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/rjant/CLionProjects/COP3530-Project-2-LoL-Game-Stats/cmake-build-debug/CMakeFiles/fc-tmp/sfml/sfml-gitupdate.cmake")
-
-endblock()
